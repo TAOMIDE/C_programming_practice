c727df85b68d874c4dc388ae3ca34f61baa251b9
Akinbami Taofeek Olamide <tfkakinbami80@gmail.com>
2022-09-21 21:08:57 +0000

    working with relational operator

---
+++ exercise_6_2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main- Further arithmetic asignment operator
+* Author- Akinbami Taofeek Olamide
+* Return: Reuslt after operation.
+*/
+
+int main(void)
+{
+	int x = 3;
+	int y = 6;
+	int z = x * y;
+
+	printf("The value of z = x * y produces: %d\n", z);
+	printf("z = x * y == 18 produces: %d\n", z = x * y == 18);
+
+	return (0);
+}
